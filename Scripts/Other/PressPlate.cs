edd70f507443b87166950078e390e12a9f6f7c03
jesse kaminsky <jck69@pitt.edu>
2017-04-02 15:56:58 -0400

    I'm a carpenter

---
+++ Scripts/Other/PressPlate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressPlate : MonoBehaviour {
+    public GameObject door;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player" || other.transform.tag == "AI")
+        {
+           door.transform.Rotate(0, -90, 0);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Player" || other.transform.tag == "AI")
+        {
+            door.transform.Rotate(0, 90, 0);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
